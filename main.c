e4bf965bf76c458ab375fb3209e42634fd300b56
poshlit <68813582+poshlit@users.noreply.github.com>
2021-04-23 22:44:31 +0500

    upload home file

---
+++ main.c
@@ -0,0 +1,21 @@
+ви#include <stdio.h>
+
+int main()
+{
+	const long int sec = 288230376151711744; // 2 ** 58
+	const float min = sec / 60;
+	const float houre = min / 60;
+	float day = houre / 24;
+	int year, log = 0;
+	
+	while (day > 0) {
+		log++;
+		if (log == 4) {
+			log = 0;
+			day -= 366; // високосный год
+		}
+		day -= 365;
+		year++;
+		printf("%d\t", year);
+	}
+}
